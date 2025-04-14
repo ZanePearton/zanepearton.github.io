5a2ea73f2f028cd8766f976533a9148304054ad8
Zane <z.pearton@accenture.com>
2025-04-14 21:46:47 +1000

    Lower latency scrolling

--- scripts.js
+++ scripts.js
@@ -83,7 +83,7 @@ window.addEventListener('scroll', () => {
         const elementHeight = element.offsetHeight;
         const windowHeight = window.innerHeight;
 
-        if (elementTop < windowHeight - elementHeight / 2) {
+        if (elementTop < windowHeight - elementHeight * 0.8) {
             element.classList.add('visible');
         }
     });
